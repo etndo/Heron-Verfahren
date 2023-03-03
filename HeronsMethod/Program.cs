2430518c889b6e49b4915b6041e4b8ee1c506a65
Etienne <Etienne.ress@gmail.com>
2023-03-03 15:29:18 +0100

    Added Visualisation

--- HeronsMethod/Program.cs
+++ HeronsMethod/Program.cs
@@ -7,25 +7,15 @@ Console.Write("Wurzel aus n: ");
 try { A = Convert.ToDouble(Console.ReadLine()); } catch { Console.WriteLine("Ungültige Eingabe!"); goto InputA; }
 double a = A;
 
-double diff;
-InputDiff:
-Console.Write("Differenz: ");
-try { diff = Convert.ToDouble(Console.ReadLine()); } catch { Console.WriteLine("Ungültige Eingabe!"); goto InputDiff; }
-
-int attempts;
-InputAttempts:
-Console.Write("Max. Versuche: ");
-try { attempts = Convert.ToInt32(Console.ReadLine()); } catch { Console.WriteLine("Ungültige Eingabe!"); goto InputAttempts; }
-
+double lasta = 0;
 double b = 1;
-int attempt = 0;
 Console.WriteLine("Berechne Wurzel aus: " + a + "...");
-while(Math.Abs(a-b) > diff && attempt < attempts)
+while(a-b != 0 /*Dont go on if solved*/ && lasta != a /*Dont loop forever*/)
 {
-    a += b; a/= 2;
+    lasta = a;
+    a = (a+b)/2;
     b = A/a;
-    attempt++;
-    Console.WriteLine("#" + attempt + ": " + "a: " + a + " b: " + b);
+    Console.WriteLine("a: " + a + " b: " + b);
 }
 Console.WriteLine("Wurzel: " + a);
 goto Start;
